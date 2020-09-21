25c3ec4b41a95a0109ccc0ddcfe103321a123b7b
Emma Song Fisher <emmasongf@gmail.com>
2020-09-20 22:26:43 -0500

    completed week 5

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -5,18 +5,30 @@ let collection = [];
 //create a variable "collection" that is an empty array
 function addToCollection( recordTitle, recordArtist, recordYear ){
   //add a function to your script named addToCollection(title, artist, year)
-  let newRecord = {
-    title: recordTitle ,
+  let record = {
+    title: recordTitle,
     artist: recordArtist,
     year: recordYear,
   }
-  console.log('add', newRecord );
-  collection.push(newRecord);
+  console.log('add', record );
+  collection.push(record);
   //when called, creates a new record object and pushes it into the "collection" array
-  return (newRecord);
-  //when called, creates a new record object and pushes it into the "collection" array
-}
+    return (record);
+  }
 
 console.log(addToCollection ( 'Week Five', 'Emma', 2020 ));
 console.log(addToCollection ( 'Snacks Plz', 'Dr Chips', 2018 ));
+console.log(addToCollection ( 'Full of Snacks', 'Dr Chips', 2021 ));
 console.log('array', collection);
+
+//I am commenting out my attempt at the first stretch so that I can check what I started with the solve video
+
+//create the function findRecord(title)
+//function findRecord( recordTitle ){
+  //if(findRecord.recordTitle( title ) === recordTitle )
+    //return indexOf( 'Title' )
+  // else return false
+//}
+
+
+//console.log(findRecord( 'Snacks Plz' ));
